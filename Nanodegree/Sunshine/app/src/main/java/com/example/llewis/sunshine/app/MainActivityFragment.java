0a265bcca4965464ad1c7a69e9b656c474c318fa
Lee A. Lewis <llewis@fullsail.com>
2015-07-06 14:00:45 -0400

    Sunshine commit 2

--- Nanodegree/Sunshine/app/src/main/java/com/example/llewis/sunshine/app/MainActivityFragment.java
+++ Nanodegree/Sunshine/app/src/main/java/com/example/llewis/sunshine/app/MainActivityFragment.java
@@ -1,10 +1,14 @@
 package com.example.llewis.sunshine.app;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -18,6 +22,17 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        String[] forecastArray = {
+            "Today - Sunny - 88 / 63",
+            "Tomorrow - Foggy - 70 / 46",
+            "Wed - Cloudy - 72 / 63",
+            "Thurs - Rain - 64 / 51",
+            "Fri - Cloudy - 72 / 63",
+            "Sat - Sunny - 85 / 73"
+        };
+
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
+
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
